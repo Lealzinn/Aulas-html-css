9b8bd3005f9f061c3fdeb2ec65c0d1e2654705f2
Gabriel Leal <121504376+Lealzinn@users.noreply.github.com>
2022-12-28 20:07:52 -0300

    Update exercicio2.js

--- Aula02/exercicio2.js
+++ Aula02/exercicio2.js
@@ -1,58 +1,64 @@
 
-/*
-let disponibilidadeLivro1 = true if (livro1 > 0) else false
-let disponibilidadeLivro2 = true if (livro2 > 0) else false
-let disponibilidadeLivro3 = true if (livro3 > 0) else false
-let disponibilidadeLivro4 = true if (livro4 > 0) else false
-let disponibilidadeLivro5 = true if (livro5 > 0) else false
-let disponibilidadeLivro6 = true if (livro6 > 0) else false
-*/
-
-
 
 const biblioteca1 = {
     nome: "1Biblioteca",
-    local:"Avenida Rio",
-    cnpj:"681293682",
-    cidade:"Rio de Janeiro",
+    local: "Avenida Rio",
+    cnpj: "681293682",
+    cidade: "Rio de Janeiro",
     estado: "RJ",
     email: "biblioteca1@gmail.com",
-    site: "biblioteca1.com.br", 
-    telefone: ["973846218","173046518"],
-    livro1: false,
-    Livro2: true
+    site: "biblioteca1.com.br",
+    telefone: ["973846218", "173046518"],
+    livros: [{
+         nome: "historiadebosque",
+         disponibilidade1: "sim" 
+        },{ 
+        nome: "mortedebosque",
+        disponibilidade2: "nao",}]
 }
 
 const biblioteca2 = {
     nome: "2Biblioteca",
-    local:"Avenida Paulista",
-    cnpj:"6818723122",
-    cidade:"São Paulo",
+    local: "Avenida Paulista",
+    cnpj: "6818723122",
+    cidade: "São Paulo",
     estado: "SP",
     email: "biblioteca2@gmail.com",
     site: "biblioteca2.com.br",
-    telefone: ["98612388","121325418"],
-    livro3: true,
-    Livro4: true
+    telefone: ["98612388", "121325418"],
+    livros: [{
+        nome: "historiadehenrique",
+        disponibilidade1: "sim", 
+    },{
+        nome: "mortehenrique",
+        disponibilidade2: "sim",
+    }]
 }
 
 const biblioteca3 = {
     nome: "3Biblioteca",
-    local:"Avenida Brasília",
-    cnpj:"6818397212",
-    cidade:"Brasília",
+    local: "Avenida Brasília",
+    cnpj: "6818397212",
+    cidade: "Brasília",
     estado: "DF",
     email: "biblioteca3@gmail.com",
     site: "biblioteca3.com.br",
-    telefone: ["4123124442","098732112"],
-    Livro5: false,
-    Livro6: false
+    telefone: ["4123124442", "098732112"],
+    livros: [{
+        nome: "historiadeleal",
+        disponibilidade1: false,
+    },{
+        nome: "mortedeleal",
+        disponibilidade2: false,
+    }]
 }
+biblioteca2.livros.forEach(function (item) {
+    item.disponibilidade1 = "não"
+})
 
-biblioteca1.Livro2 = false;
-biblioteca3.Livro5 = true;
-biblioteca1.Livro6 = true;
+biblioteca1.livros.push({
+    nome: "fimosedebosque",
+    disponibilidade: "sim",
+})
 
-
-
-
+console.log(biblioteca1, biblioteca2, biblioteca3)
